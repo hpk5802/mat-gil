f7a62a8925b29915a435f06dac0daa8219afaf63
ksm <hpk5802@github.com>
2025-01-18 22:12:50 +0900

    feat: YouTube 데이터 API 설계 및 무한 스크롤을 위한 limit 파라미터 추가

---
+++ app/api/pungja/route.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+
+interface YoutubeData {
+  id: string;
+  title: string;
+  url: string;
+}
+
+export async function GET(request: Request): Promise<Response> {
+  try {
+    // JSON 파일 경로 설정
+    const filePath = path.join(process.cwd(), 'data', 'pungja.json');
+
+    // 파일 읽기
+    const fileContents = await fs.readFile(filePath, 'utf-8');
+
+    // JSON 파싱
+    const data: YoutubeData[] = JSON.parse(fileContents);
+
+    const url = new URL(request.url);
+    const limitParam = url.searchParams.get('limit');
+    const limit = limitParam ? parseInt(limitParam, 10) : data.length;
+
+    const limitedData = data.slice(0, limit);
+
+    return new Response(JSON.stringify({ success: true, lists: limitedData }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('데이터를 받아오는 중 에러가 발생했습니다:', error);
+    return new Response(
+      JSON.stringify({ success: false, message: '데이터 요청 실패' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    );
+  }
+}
---
+++ app/pungja/page.tsx
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+async function page() {
+  const {
+    data: { lists },
+  } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/pungja`, {
+    params: {
+      limit: 10,
+    },
+  });
+
+  return (
+    <div>
+      {lists.map((list: any) => (
+        <div key={list.snippet.title}>{list.snippet.title}</div>
+      ))}
+    </div>
+  );
+}
+
+export default page;
