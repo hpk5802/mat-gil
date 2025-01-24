0c161e334edf6d6b7299786e46ba39953f173115
hpk5802 <hpk5802@gmail.com>
2025-01-24 22:54:08 +0900

    refactor: lists 타입 지정정

--- app/components/lists/ListContainer.tsx
+++ app/components/lists/ListContainer.tsx
@@ -1,6 +1,7 @@
+import { YoutubeData } from '@/app/types/youtube';
 import Card from './Card';
 
-function ListContainer({ lists }) {
+function ListContainer({ lists }: { lists: YoutubeData[] }) {
   return (
     <div>
       <div className="flex flex-wrap gap-[3%] gap-y-4 w-[640px] mx-auto">
