e8f271acd7dc34fea44250bfcbe6cfb021c6a337
hpk5802 <hpk5802@gmail.com>
2025-01-24 21:48:58 +0900

    feat: 타입 파일 분리 및 적용

--- app/components/lists/Card.tsx
+++ app/components/lists/Card.tsx
@@ -1,18 +1,20 @@
+import { YoutubeData } from '@/app/types/youtube';
 import Image from 'next/image';
 import Link from 'next/link';
 
-function Card({ lists }) {
-  return lists.map((list: any) => (
+function Card({ lists }: { lists: YoutubeData[] }) {
+  return lists.map((list) => (
     <div key={list.id} className="w-[48.5%]">
-      <Link href={`/ssg/${list.snippet.position}`}>
+      <Link href={`/ssg/${list.position}`}>
         <div className="relative w-full h-44 mb-2 rounded-lg overflow-hidden pb-2">
           <Image
-            src={list.snippet.thumbnails.medium.url}
+            src={list.thumbnailUrl}
+            className="object-cover"
             fill
             alt="thumbnail"
           />
         </div>
-        <h3 className="text-md line-clamp-2 h-12">{list.snippet.title}</h3>
+        <h3 className="text-md line-clamp-2 h-12">{list.title}</h3>
       </Link>
     </div>
   ));
