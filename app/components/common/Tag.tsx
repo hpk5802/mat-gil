40585c3cd9c66da1d3d81b5b127ad0ff490a474e
ksm <hpk5802@github.com>
2025-01-31 21:48:29 +0900

    refactor: 클래스 조건 3항 연산으로 수정

--- app/components/common/Tag.tsx
+++ app/components/common/Tag.tsx
@@ -10,9 +10,10 @@ function Tag({ children, href, isActive }: PropsWithChildren<TagInteface>) {
   return (
     <Link
       href={href}
-      className={`text-white px-2 py-1 border border-white rounded-full text-xs md:text-md md:px-3 md:py-1.5 ${
-        isActive &&
-        'bg-active-bg text-active-text font-semibold cursor-not-allowed'
+      className={`px-2 py-1 border border-white rounded-full text-xs md:text-md md:px-3 md:py-1.5 ${
+        isActive
+          ? 'bg-active-bg text-active-text font-semibold cursor-not-allowed'
+          : 'text-white'
       }`}
     >
       {children}
