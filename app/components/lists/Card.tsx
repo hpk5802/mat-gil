4515bb4de1583bf2b1d98bb5d451fab1345dfc50
ksm <hpk5802@github.com>
2025-02-05 18:20:36 +0900

    refactor: 상세 페이지 route 주소 수정

--- app/components/lists/Card.tsx
+++ app/components/lists/Card.tsx
@@ -2,13 +2,18 @@ import { YoutubeData } from '@/app/types/youtube';
 import Image from 'next/image';
 import Link from 'next/link';
 
-function Card({ lists }: { lists: YoutubeData[] }) {
+interface CardInterface {
+  channel: string;
+  lists: YoutubeData[];
+}
+
+function Card({ channel, lists }: CardInterface) {
   return lists.map((list) => (
     <div
       key={list.id}
       className="w-[48.5%] md:w-[32%] md:border-none border-gray-300 shadow-card rounded-lg overflow-hidden text-white"
     >
-      <Link href={`/ssg/${list.position}`}>
+      <Link href={`/${channel}/${list.position}`}>
         <div className="relative w-full aspect-[1.75/1]">
           <Image
             src={list.thumbnailUrl}
