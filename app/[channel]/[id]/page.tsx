2aa3266020be76dfb9a19cb59d6a5b8edd9fb4ec
ksm <hpk5802@github.com>
2025-02-12 10:12:23 +0900

    design: 상세 페이지 주소 추가

--- app/[channel]/[id]/page.tsx
+++ app/[channel]/[id]/page.tsx
@@ -1,4 +1,6 @@
+import Divider from '@/app/components/common/Divider';
 import VideoPlayer from '@/app/components/detail/VideoPlayer';
+import IconMarker from '@/app/components/icons/IconMarker';
 import axios from '@/app/lib/instance';
 
 async function DetailPage({
@@ -12,17 +14,18 @@ async function DetailPage({
     data: { list },
   } = await axios.get(`${channel}/${id}`);
 
+  const { videoId, thumbnail, timeline, address } = list;
+
   return (
-    <div className="max-w-[46.25rem] mx-auto px-3 py-5">
+    <div className="max-w-[46.25rem] mx-auto px-3 py-5 text-white">
       <div className="relative w-full aspect-[1.75/1]">
-        <VideoPlayer
-          videoId={list.videoId}
-          lazy={list.thumbnail}
-          timeline={list.timeline}
-        />
+        <VideoPlayer videoId={videoId} lazy={thumbnail} timeline={timeline} />
       </div>
-      <div className="text-white mt-3 font-semibold line-clamp-2">
-        {list.title}
+      <div className="mt-3 font-semibold line-clamp-2">{list.title}</div>
+      <Divider />
+      <div className="flex items-center gap-1">
+        <IconMarker className="w-3.5 h-3.5" />
+        {address}
       </div>
     </div>
   );
