ef323d191bbf15ed03374b96369be5e0b2ab4b93
ksm <hpk5802@github.com>
2025-02-19 20:47:26 +0900

    feat: LazyImage 컴포넌트 추가(스켈레톤 UI 공통 적용)

---
+++ app/components/common/LazyImage.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import SkeletonImage from '../Skeleton/SkeletonImage';
+
+interface LazyImageProps {
+  thumbnail: string;
+  alt: string;
+}
+
+function LazyImage({ thumbnail, alt }: LazyImageProps) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <>
+      {isLoading && <SkeletonImage />}
+      <Image
+        src={thumbnail}
+        className="object-cover transition-transform duration-300 group-hover:scale-105"
+        fill
+        alt={alt}
+        onLoad={() => setIsLoading(false)}
+      />
+    </>
+  );
+}
+
+export default LazyImage;
