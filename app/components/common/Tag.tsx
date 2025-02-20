603ce0f3fda09093454a0cadc6192be4af318128
ksm <hpk5802@github.com>
2025-02-20 14:50:59 +0900

    style: 모바일 디바이스에서 focus 스타일 적용 안되도록 수정

--- app/components/common/Tag.tsx
+++ app/components/common/Tag.tsx
@@ -10,7 +10,7 @@ function Tag({ children, href, isActive }: PropsWithChildren<TagInteface>) {
   return (
     <Link
       href={href}
-      className={`px-2 py-1.5 border border-white rounded-full flex-shrink-0 text-xs md:text-md md:px-3 focus:bg-neutral-400 focus:outline-none ${
+      className={`px-2 py-1.5 border border-white rounded-full flex-shrink-0 text-xs md:text-md md:px-3 md:focus:bg-neutral-400 focus:outline-none ${
         isActive
           ? 'bg-active-bg text-active-text font-semibold cursor-not-allowed focus:bg-active-bg'
           : 'text-white'
