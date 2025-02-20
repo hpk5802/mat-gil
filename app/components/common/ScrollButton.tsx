921a6aa7456842d7708c74302fd117223f0b2631
ksm <hpk5802@github.com>
2025-02-20 16:18:58 +0900

    refactor: 스크롤 버튼 컴포넌트로 분리

---
+++ app/components/common/ScrollButton.tsx
@@ -0,0 +1,29 @@
+import IconArrowLeft from '@/app/components/icons/IconArrowLeft';
+import IconArrowRight from '@/app/components/icons/IconArrowRight';
+
+interface ScrollButtonProps {
+  direction: 'left' | 'right';
+  scrollRef: React.RefObject<HTMLDivElement | null>;
+}
+
+function ScrollButton({ direction, scrollRef }: ScrollButtonProps) {
+  const handleSrollButtonClick = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollBy({
+        left: direction === 'left' ? -150 : 150,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  return (
+    <button
+      className="justify-center items-center shrink-0 w-7 h-7 hidden md:flex"
+      onClick={handleSrollButtonClick}
+    >
+      {direction === 'left' ? <IconArrowLeft /> : <IconArrowRight />}
+    </button>
+  );
+}
+
+export default ScrollButton;
