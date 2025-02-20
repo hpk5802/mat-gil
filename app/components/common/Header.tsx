c597a2faf6de358a0e7ad897edde9a2d0e6ccc88
ksm <hpk5802@github.com>
2025-02-20 17:59:22 +0900

    fix: 태그 active 옵션 수정

--- app/components/common/Header.tsx
+++ app/components/common/Header.tsx
@@ -3,12 +3,12 @@
 import Link from 'next/link';
 import Logo from '@/app/components/icons/Logo';
 import Tag from '@/app/components/common/Tag';
-import { usePathname } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import tags from '@/app/constants/tags';
 import TagContainer from '@/app/components/TagContainer';
 
 function Header() {
-  const path = usePathname();
+  const { channel } = useParams();
 
   return (
     <header className="fixed w-full h-fit inset-0 z-10 bg-brand-background-opacity py-1">
@@ -25,7 +25,7 @@ function Header() {
           <Tag
             key={`tag_${tag.title}`}
             href={tag.path}
-            isActive={tag.path === path}
+            isActive={tag.path === `/${channel}`}
           >
             {tag.title}
           </Tag>
