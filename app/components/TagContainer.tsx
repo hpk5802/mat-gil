4d9e4028486824ee47996ef21e88b6da57f7ff45
ksm <hpk5802@github.com>
2025-02-20 16:21:52 +0900

    refactor: tab 접근 고려 버튼 위치 수정

--- app/components/TagContainer.tsx
+++ app/components/TagContainer.tsx
@@ -1,30 +1,11 @@
 import { PropsWithChildren, useRef } from 'react';
-import IconArrowLeft from '@/app/components/icons/IconArrowLeft';
-import IconArrowRight from '@/app/components/icons/IconArrowRight';
+import ScrollButton from '@/app/components/common/ScrollButton';
 
 function TagContainer({ children }: PropsWithChildren) {
-  const scrollRef = useRef<HTMLDivElement>(null);
-
-  const scrollLeft = () => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollBy({ left: -200, behavior: 'smooth' });
-    }
-  };
-
-  const scrollRight = () => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollBy({ left: 200, behavior: 'smooth' });
-    }
-  };
+  const scrollRef = useRef<HTMLDivElement | null>(null);
 
   return (
-    <div className="flex gap-2 items-center max-w-[46.25rem] mx-auto mt-3">
-      <button
-        className="flex justify-center items-center shrink-0 w-7 h-7"
-        onClick={scrollLeft}
-      >
-        <IconArrowLeft />
-      </button>
+    <div className="flex gap-2 items-center max-w-[46.25rem] mx-auto mt-3 px-3 md:px-0">
       <div
         ref={scrollRef}
         className="flex flex-nowrap touch-pan-x gap-x-2 md:overflow-hidden overflow-x-scroll hide-scroll"
@@ -33,12 +14,8 @@ function TagContainer({ children }: PropsWithChildren) {
       >
         {children}
       </div>
-      <button
-        className="flex justify-center items-center shrink-0 w-7 h-7"
-        onClick={scrollRight}
-      >
-        <IconArrowRight />
-      </button>
+      <ScrollButton direction="left" scrollRef={scrollRef} />
+      <ScrollButton direction="right" scrollRef={scrollRef} />
     </div>
   );
 }
