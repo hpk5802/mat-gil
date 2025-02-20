f3f04f48e770813083687ad6d4c91aabd3924fc6
ksm <hpk5802@github.com>
2025-02-20 15:07:43 +0900

    feat: 스크린 리더 고려 role, aria 속성 추가

--- app/components/common/Tag.tsx
+++ app/components/common/Tag.tsx
@@ -12,10 +12,13 @@ function Tag({ children, href, isActive }: PropsWithChildren<TagInteface>) {
       href={href}
       className={`px-2 py-1.5 border border-white rounded-full flex-shrink-0 text-xs md:text-md md:px-3 md:focus:bg-neutral-400 focus:outline-none ${
         isActive
-          ? 'bg-active-bg text-active-text font-semibold cursor-not-allowed focus:bg-active-bg'
+          ? 'bg-active-bg text-active-text font-semibold focus:bg-active-bg pointer-events-none'
           : 'text-white'
       }`}
       tabIndex={isActive ? -1 : 0}
+      role="link"
+      aria-current={isActive ? 'page' : undefined}
+      aria-disabled={isActive ? true : undefined}
     >
       {children}
     </Link>
