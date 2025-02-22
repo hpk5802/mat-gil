9dcce36898e4f160e6a13e16961fbc1f4b3a33d7
ksm <hpk5802@github.com>
2025-02-22 10:29:26 +0900

    feat: Dialog backdrop 영역 클릭 시 닫힘 기능 추가

--- app/components/common/Dialog.tsx
+++ app/components/common/Dialog.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { PropsWithChildren, Ref } from 'react';
+import { MouseEvent, PropsWithChildren, Ref } from 'react';
 import IconClose from '@/app/components/icons/IconClose';
 
 interface DialogProps {
@@ -9,19 +9,28 @@ interface DialogProps {
 }
 
 function Dialog({ ref, handleClose }: PropsWithChildren<DialogProps>) {
+  const handleCloseDialog = (e: MouseEvent<HTMLDialogElement>) => {
+    if (e.target === e.currentTarget) {
+      handleClose();
+    }
+  };
+
   return (
     <dialog
       ref={ref}
-      className="open:animate-show-dialog open:backdrop:animate-show-backdrop bottom-0 top-auto mx-0 my-0 max-h-[80%] w-full max-w-none transform rounded-t-xl bg-dialog-background p-3 pt-8 backdrop:bg-dialog-backdrop md:inset-y-0 md:mx-auto md:my-auto md:w-96 md:rounded-b-xl"
+      className="open:animate-show-dialog open:backdrop:animate-show-backdrop bottom-0 top-auto mx-0 my-0 max-h-[80%] w-full max-w-none transform rounded-t-xl bg-dialog-background backdrop:bg-dialog-backdrop md:inset-y-0 md:mx-auto md:my-auto md:w-96 md:rounded-b-xl"
+      onClick={handleCloseDialog}
     >
-      <button
-        type="button"
-        className="absolute right-3 top-3 flex h-5 w-5 items-center justify-center"
-        autoFocus
-        onClick={handleClose}
-      >
-        <IconClose className="h-[75%] w-[75%]" />
-      </button>
+      <div className="p-3 pt-8">
+        <button
+          type="button"
+          className="absolute right-3 top-3 flex h-5 w-5 items-center justify-center"
+          autoFocus
+          onClick={handleClose}
+        >
+          <IconClose className="h-[75%] w-[75%]" />
+        </button>
+      </div>
     </dialog>
   );
 }
