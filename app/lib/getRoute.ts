7aa37a01c224ef983ef3b7d9fb6b60b74472d37e
ksm <hpk5802@github.com>
2025-02-22 18:41:28 +0900

    refactor: 길찾기 API response 타입 지정

--- app/lib/getRoute.ts
+++ app/lib/getRoute.ts
@@ -1,7 +1,25 @@
 import axios from '@/app/lib/instance';
 
+type Coordinates = [number, number];
+
+type Direction = {
+  path: Coordinates[];
+  summary: {
+    distance: number; // 거리 (m)
+    duration: number; // 소요 시간 (ms)
+    taxiFare: number; // 택시 요금
+    tollFare: number; // 통행료
+  };
+};
+
+type RouteResponse = {
+  route: {
+    traoptimal: Direction[];
+  };
+};
+
 const getRoute = async (start: string, goal: string) => {
-  const res = await axios.get('directions', {
+  const res = await axios.get<RouteResponse>('directions', {
     params: { start, goal },
   });
 
