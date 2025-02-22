60692042b0bd9223d95ead210d15fe602fcff0bd
ksm <hpk5802@github.com>
2025-02-22 20:02:47 +0900

    feat: 길찾기 Loading & Error state 추가

--- app/components/direction/DirectionWrap.tsx
+++ app/components/direction/DirectionWrap.tsx
@@ -10,6 +10,7 @@ import { Direction } from '@/app/types/directions';
 function DirectionWrap() {
   const { dialogRef, openDialog, closeDialog } = useDialog();
   const [directions, setDirections] = useState<Direction | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSuccess = async (position: GeolocationPosition) => {
     const latitude = position.coords.latitude;
@@ -17,12 +18,19 @@ function DirectionWrap() {
     const start = `${longitude},${latitude}`;
     const goal = sessionStorage.getItem('destination') || '';
 
-    const {
-      route: { traoptimal },
-    } = await getRoute(start, goal);
-
-    setDirections(traoptimal[0]);
+    setIsLoading(true);
     openDialog();
+
+    try {
+      const {
+        route: { traoptimal },
+      } = await getRoute(start, goal);
+      setDirections(traoptimal[0]);
+    } catch (error) {
+      console.error('길찾기 실패: error');
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const handleError = (error: GeolocationPositionError) => {
@@ -41,7 +49,9 @@ function DirectionWrap() {
         길찾기
       </button>
       <Dialog ref={dialogRef} title="길찾기 모달" handleClose={closeDialog}>
-        {directions ? (
+        {isLoading ? (
+          <div className="h-[25rem] w-full">길찾기 정보를 불러오는 중...</div>
+        ) : directions ? (
           <div>
             <div>거리: {directions.summary.distance}</div>
             <div>예상 시간: {directions.summary.duration}</div>
@@ -50,7 +60,7 @@ function DirectionWrap() {
             <DirectionMap path={directions.path} />
           </div>
         ) : (
-          <div>길찾기 정보를 불러오는 중...</div>
+          <div className="h-[25rem] w-full">길찾기에 실패했습니다.</div>
         )}
       </Dialog>
     </>
