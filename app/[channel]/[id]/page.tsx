1c1987472a26ac059750850d8e5ae8b7c990fe38
ksm <hpk5802@github.com>
2025-02-22 16:45:12 +0900

    feat: 상세 페이지 길찾기 버튼 추가

--- app/[channel]/[id]/page.tsx
+++ app/[channel]/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Divider from '@/app/components/common/Divider';
 import VideoPlayer from '@/app/components/detail/VideoPlayer';
+import DirectionWrap from '@/app/components/direction/DirectionWrap';
 import IconMarker from '@/app/components/icons/IconMarker';
 import Map from '@/app/components/Map/Map';
 import axios from '@/app/lib/instance';
@@ -18,23 +19,26 @@ async function DetailPage({
   const { videoId, thumbnail, timeline, address, title } = list;
 
   return (
-    <div className="mx-auto max-w-[46.25rem] px-3 py-5 text-white">
-      <div className="relative aspect-[1.75/1] w-full overflow-hidden">
-        <VideoPlayer
-          videoId={videoId}
-          lazy={thumbnail}
-          timeline={timeline}
-          title={title}
-        />
+    <>
+      <div className="mx-auto max-w-[46.25rem] px-3 py-5 text-white">
+        <div className="relative aspect-[1.75/1] w-full overflow-hidden">
+          <VideoPlayer
+            videoId={videoId}
+            lazy={thumbnail}
+            timeline={timeline}
+            title={title}
+          />
+        </div>
+        <div className="mt-3 line-clamp-2 font-semibold">{list.title}</div>
+        <Divider />
+        <div className="mb-2 flex items-center gap-1">
+          <IconMarker className="h-3.5 w-3.5" />
+          {address}
+        </div>
+        <DirectionWrap />
+        <Map address={address} />
       </div>
-      <div className="mt-3 line-clamp-2 font-semibold">{list.title}</div>
-      <Divider />
-      <div className="mb-2 flex items-center gap-1">
-        <IconMarker className="h-3.5 w-3.5" />
-        {address}
-      </div>
-      {/* <Map address={address} /> */}
-    </div>
+    </>
   );
 }
 
