5724773f3ab12c56bbf90ac9aba6c6b85f8958fd
ksm <hpk5802@github.com>
2025-02-22 13:26:57 +0900

    refactor: 배포 시 환경변수 클라이언트에서 접근 못하도록 NEXT_PUBLIC 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         <Script
           strategy="beforeInteractive"
           type="text/javascript"
-          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&submodules=geocoder`}
+          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NAVER_CLIENT_ID}&submodules=geocoder`}
         />
         <Header />
         {children}
