e8dc1efa441376bf42e00ef26cb63b72dac0a172
ksm <hpk5802@github.com>
2025-02-22 10:09:57 +0900

    feat: 닫기 버튼 아이콘 컴포넌트 추가

---
+++ app/components/icons/IconClose.tsx
@@ -0,0 +1,27 @@
+function IconClose({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className || ''}
+      width="10"
+      height="10"
+      viewBox="0 0 10 10"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.08008 1L9.08008 9"
+        stroke="white"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+      />
+      <path
+        d="M9 1L1 9"
+        stroke="white"
+        strokeWidth="1.8"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
+
+export default IconClose;
