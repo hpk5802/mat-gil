c76c712e6d3e5cdb545703b797534c0948ddd9c2
ksm <hpk5802@github.com>
2025-02-22 10:10:21 +0900

    feat: Dialog(모달) 컴포넌트 추가

--- app/components/common/Dialog.tsx
+++ app/components/common/Dialog.tsx
@@ -1,20 +1,27 @@
 'use client';
 
-import { Ref } from 'react';
+import { PropsWithChildren, Ref } from 'react';
+import IconClose from '@/app/components/icons/IconClose';
 
 interface DialogProps {
   ref: Ref<HTMLDialogElement>;
   handleClose: () => void;
 }
 
-function Dialog({ ref, handleClose }: DialogProps) {
+function Dialog({ ref, handleClose }: PropsWithChildren<DialogProps>) {
   return (
-    <dialog ref={ref}>
-      <button type="button" autoFocus onClick={handleClose}>
-        x
+    <dialog
+      ref={ref}
+      className="open:animate-show-dialog open:backdrop:animate-show-backdrop bottom-0 top-auto mx-0 my-0 max-h-[80%] w-full max-w-none transform rounded-t-xl bg-dialog-background p-3 pt-8 backdrop:bg-dialog-backdrop md:inset-y-0 md:mx-auto md:my-auto md:w-96 md:rounded-b-xl"
+    >
+      <button
+        type="button"
+        className="absolute right-3 top-3 flex h-5 w-5 items-center justify-center"
+        autoFocus
+        onClick={handleClose}
+      >
+        <IconClose className="h-[75%] w-[75%]" />
       </button>
-      <h2>모달 타이틀</h2>
-      <div>모달 콘텐츠</div>
     </dialog>
   );
 }
