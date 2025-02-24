a023c883a00d13abe3ea7ac910eb1b2fcbe84a99
ksm <hpk5802@github.com>
2025-02-24 14:57:29 +0900

    design: 아이콘 추가 및 버튼 스타일 수정

--- app/components/direction/DirectionWrap.tsx
+++ app/components/direction/DirectionWrap.tsx
@@ -11,6 +11,11 @@ import {
   convertMilliSecondsToTime,
   convertPriceToWon,
 } from '@/app/utils/format';
+import IconMarker from '@/app/components/icons/IconMarker';
+import IconClock from '@/app/components/icons/IconClock';
+import IconCharge from '@/app/components/icons/IconCharge';
+import IconHandle from '@/app/components/icons/IconHandle';
+import IconNavigation from '../icons/IconNavigation';
 
 function DirectionWrap() {
   const { dialogRef, openDialog, closeDialog } = useDialog();
@@ -50,35 +55,52 @@ function DirectionWrap() {
 
   return (
     <>
-      <button type="button" onClick={handleClick}>
+      <button
+        type="button"
+        className="my-2 flex h-11 w-full items-center justify-center gap-1 rounded-lg bg-emerald-600 font-semibold"
+        onClick={handleClick}
+      >
+        <IconNavigation className="h-4 w-4" />
         길찾기
       </button>
       <Dialog ref={dialogRef} title="길찾기 모달" handleClose={closeDialog}>
         {isLoading ? (
-          <div className="h-[27.5rem] w-full">길찾기 정보를 불러오는 중...</div>
+          <div className="h-[28.5rem] w-full">길찾기 정보를 불러오는 중...</div>
         ) : directions ? (
           <>
             <div className="mb-2 mt-2 grid grid-cols-2 gap-2">
               <div className="flex flex-col">
-                <span className="text-md text-gray-300">거리</span>
+                <span className="flex items-center gap-0.5 text-md text-gray-300">
+                  <IconMarker className="h-3 w-3" />
+                  거리
+                </span>
                 <span className="font-semibold text-white">
                   {convertMetersToKilometers(directions.summary.distance)}
                 </span>
               </div>
               <div className="flex flex-col">
-                <span className="text-md text-gray-300">예상 시간</span>
+                <span className="flex items-center gap-0.5 text-md text-gray-300">
+                  <IconClock className="h-3 w-3" />
+                  예상 시간
+                </span>
                 <span className="font-semibold text-white">
                   {convertMilliSecondsToTime(directions.summary.duration)}
                 </span>
               </div>
               <div className="flex flex-col">
-                <span className="text-md text-gray-300">택시요금</span>
+                <span className="flex items-center gap-0.5 text-md text-gray-300">
+                  <IconHandle className="h-3 w-3" />
+                  택시요금
+                </span>
                 <span className="font-semibold text-white">
                   {convertPriceToWon(directions.summary.taxiFare)}
                 </span>
               </div>
               <div className="flex flex-col">
-                <span className="text-md text-gray-300">통행료</span>
+                <span className="flex items-center gap-0.5 text-md text-gray-300">
+                  <IconCharge className="h-3 w-3" />
+                  통행료
+                </span>
                 <span className="font-semibold text-white">
                   {convertPriceToWon(directions.summary.tollFare)}
                 </span>
@@ -87,7 +109,7 @@ function DirectionWrap() {
             <DirectionMap path={directions.path} />
           </>
         ) : (
-          <div className="h-[27.5rem] w-full">길찾기에 실패했습니다.</div>
+          <div className="h-[28.5rem] w-full">길찾기에 실패했습니다.</div>
         )}
       </Dialog>
     </>
