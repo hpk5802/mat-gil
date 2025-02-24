6f8503f78b4be044d07c79330c7d97c5daafba28
ksm <hpk5802@github.com>
2025-02-24 15:14:08 +0900

    refactor: Summary 타입 분리

--- app/types/directions.ts
+++ app/types/directions.ts
@@ -1,13 +1,15 @@
 type Coordinates = [number, number];
 
-type Direction = {
-  path: Coordinates[];
-  summary: {
-    distance: number; // 거리 (m)
-    duration: number; // 소요 시간 (ms)
-    taxiFare: number; // 택시 요금
-    tollFare: number; // 통행료
-  };
+type Summary = {
+  distance: number; // 거리 (m)
+  duration: number; // 소요 시간 (ms)
+  taxiFare: number; // 택시 요금
+  tollFare: number; // 통행료
 };
 
-export type { Coordinates, Direction };
+type Direction = {
+  path: Coordinates[];
+  summary: Summary;
+};
+
+export type { Coordinates, Summary, Direction };
