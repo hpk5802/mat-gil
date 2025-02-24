494c68dda82020f3a4cc93c9d709694235007fcf
ksm <hpk5802@github.com>
2025-02-24 20:53:30 +0900

    refactor: 아이콘 className Props 추가

--- app/components/icons/IconArrowRight.tsx
+++ app/components/icons/IconArrowRight.tsx
@@ -1,6 +1,7 @@
-function IconArrowRight() {
+function IconArrowRight({ className }: { className?: string }) {
   return (
     <svg
+      className={className || ''}
       width="20"
       height="20"
       viewBox="0 0 20 20"
