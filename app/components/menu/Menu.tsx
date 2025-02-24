edfd60d849c5aefc17e49e9826b79d32a51863e2
ksm <hpk5802@github.com>
2025-02-24 22:50:46 +0900

    refactor: menu string[] 타입에 맞게 컴포넌트 수정

--- app/components/menu/Menu.tsx
+++ app/components/menu/Menu.tsx
@@ -1,23 +1,16 @@
 import IconArrowRight from '@/app/components/icons/IconArrowRight';
 
-function Menu({ menu }: { menu: string | string[] }) {
+function Menu({ menu }: { menu: string[] }) {
   return (
     <div>
       <h3>영상 속 소개 메뉴</h3>
       <ul>
-        {menu instanceof Array ? (
-          menu.map((food) => (
-            <li key={food} className="flex items-center gap-1">
-              <IconArrowRight className="h-4 w-4" />
-              <span tabIndex={0}>{food}</span>
-            </li>
-          ))
-        ) : (
-          <li className="flex items-center gap-1">
+        {menu.map((food) => (
+          <li key={food} className="flex items-center gap-1">
             <IconArrowRight className="h-4 w-4" />
-            <span tabIndex={0}>{menu}</span>
+            <span tabIndex={0}>{food}</span>
           </li>
-        )}
+        ))}
       </ul>
     </div>
   );
