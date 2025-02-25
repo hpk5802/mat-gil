e8fa757593495dc8e9f901e83657bb6f139087e2
ksm <hpk5802@github.com>
2025-02-25 10:39:26 +0900

    feat: 재시도, 안내 아이콘 컴포넌트 추가

---
+++ app/components/icons/IconRetry.tsx
@@ -0,0 +1,29 @@
+function IconRetry({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className || ''}
+      version="1.0"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24.000000pt"
+      height="24.000000pt"
+      viewBox="0 0 24.000000 24.000000"
+      preserveAspectRatio="xMidYMid meet"
+      aria-hidden="true"
+    >
+      <g
+        transform="translate(0.000000,24.000000) scale(0.100000,-0.100000)"
+        fill="#ffffff"
+        stroke="none"
+      >
+        <path
+          d="M51 206 c-47 -26 -63 -84 -36 -135 15 -31 65 -57 90 -47 20 7 10 26
+-14 26 -25 0 -61 42 -61 71 0 50 63 84 109 59 25 -13 36 -42 14 -38 -29 5 -24
+-17 9 -42 l31 -23 24 29 c28 35 29 48 3 41 -13 -3 -21 1 -25 14 -16 50 -92 74
+-144 45z"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default IconRetry;
