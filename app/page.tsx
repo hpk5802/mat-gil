aebbbcab672d8f86b86ca0db5065c63613a570d7
ksm <hpk5802@github.com>
2025-03-11 20:44:57 +0900

    feat: 채널 목록 섹션 추가 - 채널 소개 및 페이지 이동을 위한 영역 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import channelList from '@/app/constants/channelList';
+import LazyImage from '@/app/components/common/LazyImage';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="mx-auto flex min-h-screen flex-wrap gap-x-[3%] gap-y-4 px-3 py-5 text-white md:w-[46.25rem] md:gap-x-[2%] md:gap-y-5 md:px-0">
@@ -13,6 +17,31 @@ export default function Home() {
           <br />
           특별한 맛의 여정이 여러분을 기다리고 있습니다!
         </p>
+      </section>
+      <section className="w-ful" aria-labelledby="channel-title">
+        <h2 id="channel-title" className="text-xl">
+          채널 목록
+        </h2>
+        <ul className="flex flex-wrap gap-x-[3%] gap-y-4">
+          {channelList.map(({ key, name, image }) => (
+            <li
+              className="relative w-64 bg-card-background"
+              key={`channel_${key}`}
+            >
+              <Link href={`/${key}`} aria-label={`${name} 채널 보러가기`}>
+                <div className="relative h-36 w-full">
+                  <LazyImage
+                    thumbnail={image}
+                    alt={`채널 썸네일: ${name}`}
+                    sizes="260px"
+                  />
+                </div>
+                <h3 className="p-2 text-lg font-semibold text-white">{name}</h3>
+                <p className="sr-only">{name} 채널의 맛집을 확인해보세요.</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </section>
     </main>
   );
