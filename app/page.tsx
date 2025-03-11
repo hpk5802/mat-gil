afd768bb71a5500d269c3fa8094f87284b4c1473
ksm <hpk5802@github.com>
2025-03-11 21:36:59 +0900

    refactor: 메인 페이지 레이아웃 수정 - main 태그 min-height 삭제 - main 태그 gap 수정 - 텍스트 사이즈 및 두께 수정

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChannelList from '@/app/components/landing/ChannelList';
 
 export default function Home() {
   return (
-    <main className="mx-auto flex min-h-screen flex-wrap gap-x-[3%] gap-y-4 px-3 py-5 text-white md:w-[46.25rem] md:gap-x-[2%] md:gap-y-5 md:px-0">
+    <main className="mx-auto flex flex-wrap gap-y-6 px-3 py-5 text-white md:w-[46.25rem] md:px-0">
       <section className="w-full text-center" aria-labelledby="main-title">
         <h1 id="main-title" className="mb-4 text-6xl font-bold text-orange-400">
           맛길
@@ -10,14 +10,14 @@ export default function Home() {
             당신의 맛있는 여정
           </span>
         </h1>
-        <p className="mb-4 text-xl text-gray-400">
+        <p className="mb-4 text-lg text-gray-400 md:text-xl">
           맛길을 따라 숨겨진 미식 세계를 경험해보세요.
           <br />
           특별한 맛의 여정이 여러분을 기다리고 있습니다!
         </p>
       </section>
-      <section className="w-full" aria-labelledby="channel-title">
-        <h2 id="channel-title" className="text-xl">
+      <section className="relative w-full" aria-labelledby="channel-title">
+        <h2 id="channel-title" className="text-xl font-semibold">
           채널 목록
         </h2>
         <ChannelList />
