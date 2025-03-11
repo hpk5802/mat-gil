bc02ccea2d30f11d12dd37176fc4721549d4d2cb
ksm <hpk5802@github.com>
2025-03-11 20:32:14 +0900

    feat: 메인(랜딩) 페이지 타이틀 섹션 추가 - 서비스 소개를 위한 타이틀 영역 추가 - 서비스 이름, 슬로건

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      MATGIL(맛길)
-    </div>
+    <main className="mx-auto flex min-h-screen flex-wrap gap-x-[3%] gap-y-4 px-3 py-5 text-white md:w-[46.25rem] md:gap-x-[2%] md:gap-y-5 md:px-0">
+      <section className="w-full text-center" aria-labelledby="main-title">
+        <h1 id="main-title" className="mb-4 text-6xl font-bold text-orange-400">
+          맛길
+          <span className="mt-2 block text-2xl font-normal text-gray-300">
+            당신의 맛있는 여정
+          </span>
+        </h1>
+        <p className="mb-4 text-xl text-gray-400">
+          맛길을 따라 숨겨진 미식 세계를 경험해보세요.
+          <br />
+          특별한 맛의 여정이 여러분을 기다리고 있습니다!
+        </p>
+      </section>
+    </main>
   );
 }
