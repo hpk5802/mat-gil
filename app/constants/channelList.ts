4e4884443a3308cf92876e59f6b9cf9b78defef0
ksm <hpk5802@github.com>
2025-03-11 20:37:45 +0900

    feat: 채널 목록 constant 추가 - 메인 페이지에서 채널 목록을 렌더링할 때 사용하기 위해 channelList 추가 - 유지보수를 위해 별도의 파일로 분리

---
+++ app/constants/channelList.ts
@@ -0,0 +1,25 @@
+const channelList = [
+  { key: 'pungja', name: '풍자 | 또간집', image: '/images/img_pungja.png' },
+  {
+    key: 'seongsigyeong',
+    name: '성시경 | 먹을텐데',
+    image: '/images/img_seongsigyeong.png',
+  },
+  {
+    key: 'hongseokcheon',
+    name: '홍석척&이원일 | 미식은 경험이다',
+    image: '/images/img_hongseokcheon.png',
+  },
+  {
+    key: 'haennim',
+    name: '입짧은 햇님 | 줄서는 식당',
+    image: '/images/img_haennim.png',
+  },
+  {
+    key: 'baekjongwon',
+    name: '백종원 | 님아 그 시장을 가오',
+    image: '/images/img_baekjongwon.png',
+  },
+];
+
+export default channelList;
