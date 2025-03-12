1c3863ccf2a0adf57264ad14c744678a35d80e46
ksm <hpk5802@github.com>
2025-03-12 17:22:37 +0900

    fix: 리치 검색결과 경고 해결 - `video 속성은 스키마(예: schema.org)에서 Restaurant 유형의 객체로 인정되지 않는 속성입니다` 경고 발생 - video 속성 삭제

--- app/components/seo/ListSchema.tsx
+++ app/components/seo/ListSchema.tsx
@@ -11,41 +11,22 @@ function ListSchema({ channel, lists }: ListSchemaProps) {
     '@context': 'https://schema.org',
     '@type': 'ItemList',
     name: `${channelMap[channel]} 맛집 리스트`,
-    itemListElement: lists.map((item, index) => {
-      const category = Array.isArray(item.category)
-        ? item.category.join(', ')
-        : item.category;
-      return {
-        '@type': 'ListItem',
-        position: index + 1,
-        item: {
-          '@type': 'Restaurant',
-          name: item.title,
-          description: `${item.title}의 메뉴와 위치를 확인하고, 길찾기 기능을 통해 해당 매장까지 간편하게 찾아가세요!`,
-          image: item.thumbnail,
-          address: {
-            '@type': 'PostalAddress',
-            addressCountry: 'KR',
-            addressLocality: item.location,
-            streetAddress: item.address,
-          },
-          video: {
-            '@type': 'VideoObject',
-            name: item.title,
-            description: `${category} - ${item.location}`,
-            thumbnailUrl: item.thumbnail,
-            uploadDate: new Date().toISOString(),
-            contentUrl: `https://mat-gil.vercel.app/${channel}/${item.position}`,
-            embedUrl: `https://mat-gil.vercel.app/${channel}/${item.position}`,
-            keywords: category,
-            contentLocation: {
-              '@type': 'Place',
-              name: item.address,
-            },
-          },
+    itemListElement: lists.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@type': 'Restaurant',
+        name: item.title,
+        description: `${item.title}의 메뉴와 위치를 확인하고, 길찾기 기능을 통해 해당 매장까지 간편하게 찾아가세요!`,
+        image: item.thumbnail,
+        address: {
+          '@type': 'PostalAddress',
+          addressCountry: 'KR',
+          addressLocality: item.location,
+          streetAddress: item.address,
         },
-      };
-    }),
+      },
+    })),
   };
 
   return (
