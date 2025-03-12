4d63a611d78f14f0349ae3cd238b13becab3617f
ksm <hpk5802@github.com>
2025-03-12 11:57:41 +0900

    fix: 이미지 확장자 수정 - 파일 확장자에 맞게 image를 `~.PNG`로 수정

--- app/constants/channelList.ts
+++ app/constants/channelList.ts
@@ -1,24 +1,33 @@
 const channelList = [
-  { key: 'pungja', name: '풍자 | 또간집', image: '/images/img_pungja.png' },
+  {
+    key: 'pungja',
+    name: '풍자 | 또간집',
+    image: '/images/img_pungja.PNG',
+    count: 202,
+  },
   {
     key: 'seongsigyeong',
     name: '성시경 | 먹을텐데',
-    image: '/images/img_seongsigyeong.png',
+    image: '/images/img_seongsigyeong.PNG',
+    count: 143,
   },
   {
     key: 'hongseokcheon',
     name: '홍석척&이원일 | 미식은 경험이다',
-    image: '/images/img_hongseokcheon.png',
+    image: '/images/img_hongseokcheon.PNG',
+    count: 94,
   },
   {
     key: 'haennim',
     name: '입짧은 햇님 | 줄서는 식당',
-    image: '/images/img_haennim.png',
+    image: '/images/img_haennim.PNG',
+    count: 14,
   },
   {
     key: 'baekjongwon',
     name: '백종원 | 님아 그 시장을 가오',
-    image: '/images/img_baekjongwon.png',
+    image: '/images/img_baekjongwon.PNG',
+    count: 44,
   },
 ];
 
